ca88ce3c84a4616e668b461425333dd6843779a8
Kavindu Herath <kavinduherath350@gmail.com>
2022-04-04 14:35:22 +0530

    Madushan Order Part

--- admin/index.php
+++ admin/index.php
@@ -20,29 +20,82 @@
 
 
                 ?>
+                <br><br>
 
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+
+                    <?php 
+                        //Sql query
+                        $sql = "SELECT * FROM tbl_category";
+                        //Excecute category
+                        $res = mysqli_query($conn, $sql);
+                        //Count the rows
+                        $count = mysqli_num_rows($res);
+                    
+                    
+                    ?>
+
+                    <h1><?php echo $count; ?></h1>
                     <br>
                     Categories
                 </div>
 
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+
+                    <?php 
+                        //Sql query
+                        $sql2 = "SELECT * FROM tbl_food";
+                        //Excecute category
+                        $res2 = mysqli_query($conn, $sql2);
+                        //Count the rows
+                        $count2 = mysqli_num_rows($res2);                   
+                    
+                    ?>
+
+
+                    <h1><?php echo $count2; ?></h1>
                     <br>
-                    Categories
+                    Foods
                 </div>
 
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+
+                     <?php 
+                        //Sql query
+                        $sql3 = "SELECT * FROM tbl_order";
+                        //Excecute category
+                        $res3 = mysqli_query($conn, $sql3);
+                        //Count the rows
+                        $count3 = mysqli_num_rows($res3);                   
+                    
+                    ?>
+
+                    <h1><?php echo $count3; ?></h1>
                     <br>
-                    Categories
+                    Total Orders
                 </div>
 
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+
+                     <?php 
+                        //Create sql query to get total revenue generated
+                        //Aggreagate function in sql
+                        $sql4 = "SELECT SUM(total) AS Total FROM tbl_order WHERE status='Delivered'";
+                        
+                        //Excecute ther order
+                        $res4 = mysqli_query($conn, $sql4);
+
+                        //Get the value
+                        $row4 = mysqli_fetch_assoc($res4);
+
+                        //Get the total revenue
+                        $total_revenue = $row4['Total'];
+                    
+                    ?>
+
+                    <h1>Rs.<?php echo $total_revenue; ?></h1>
                     <br>
-                    Categories
+                    Revenue Generated
                 </div>
 
                 <div class="clearfix"></div>
--- admin/manage-order.php
+++ admin/manage-order.php
@@ -5,46 +5,123 @@
         
         <h1>Manage Order</h1>
 
-                <br> <br> 
+                <br> <br> <br>
+
+                <?php
+                    if(isset($_SESSION['update']))
+                    {
+                        echo $_SESSION['update'];
+                        unset($_SESSION['update']);
+                    }
+                
+                ?>
+
+                <br> <br>
 
                 <table class="tbl-full">
                     <tr>
                         <th>S.N.</th>
-                        <th>Full Name</th>
-                        <th>Username</th>
+                        <th>Food</th>
+                        <th>Price</th>
+                        <th>Qty.</th>
+                        <th>Total</th>
+                        <th>Order Date</th>
+                        <th>Status</th>
+                        <th>Customer Name</th>
+                        <th>Contact</th>
+                        <th>Email</th>
+                        <th>Address</th>
                         <th>Actions</th>
 
                     </tr>
 
-                    <tr>
-                        <td>1.</td>
-                        <td>Kavindu Herath</td>
-                        <td>KattaBB</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
+                    <?php 
+                        //Get all the orders from database
+                        $sql = "SELECT * FROM tbl_order ORDER BY id DESC"; //Display the latest order at first
+                        //Excecute query
+                        $res = mysqli_query($conn, $sql);
+                        //Count the rows
+                        $count = mysqli_num_rows($res);
 
-                    <tr>
-                        <td>2.</td>
-                        <td>Kavindu Herath</td>
-                        <td>KattaBB</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
+                        $sn = 1; //Create serial number and set its initial value as 1
 
-                    <tr>
-                        <td>3.</td>
-                        <td>Kavindu Herath</td>
-                        <td>KattaBB</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
+
+                        if($count>0)
+                        {
+                            //Order Available
+                            while($row=mysqli_fetch_assoc($res))
+                            {
+                                //Get all the order details
+                                $id = $row['id'];
+                                $food = $row['food'];
+                                $price = $row['price'];
+                                $qty = $row['qty'];
+                                $total = $row['total'];
+                                $order_date = $row['order_date'];
+                                $status = $row['status'];
+                                $customer_name = $row['customer_name'];
+                                $customer_contact = $row['customer_contact'];
+                                $customer_email = $row['customer_email'];
+                                $customer_address = $row['customer_address'];
+
+                                ?>
+
+                                    <tr>
+                                        <td><?php echo $sn++;?>.</td>
+                                        <td><?php echo $food;?></td>
+                                        <td><?php echo $price;?></td>
+                                        <td><?php echo $qty;?></td>
+                                        <td><?php echo $total;?></td>
+                                        <td><?php echo $order_date;?></td>
+
+
+                                        <td>
+                                            <?php
+                                                //Ordered, On Delivery, Delivered, Cancelled
+                                                if($status=="Ordered")
+                                                {
+                                                    echo "<label>$status</label>";
+                                                }
+                                                else if($status=="On Delivery")
+                                                {
+                                                    echo "<label style='color: orange;'>$status</label>";
+                                                }
+                                                else if($status=="Delivered")
+                                                {
+                                                    echo "<label style='color: Green;'>$status</label>";
+                                                }
+                                                else if($status=="Cancelled")
+                                                {
+                                                    echo "<label style='color: red;'>$status</label>";
+                                                }
+                                            ?>
+                                        </td>
+
+
+                                        <td><?php echo $customer_name;?></td>
+                                        <td><?php echo $customer_contact;?></td>
+                                        <td><?php echo $customer_email;?></td>
+                                        <td><?php echo $customer_address;?></td>
+                                        <td>
+                                            <a href="<?php echo SITEURL; ?>admin/update-order.php?id=<?php echo $id; ?>" class="btn-secondary">Update Order</a>
+                                        </td>
+                                    </tr>
+
+                                <?php
+
+                            }
+                        }
+                        else
+                        {
+                            //Order not available
+                            echo "<tr><td colspan='12' class='error'>Orders not Avilable.</td></tr>";
+                        }
+                    
+                    ?>
+
+                   
+
+                   
 
                 </table>
 
