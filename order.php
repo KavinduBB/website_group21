344fd19e4a080a85adfe439342270ebde8cd2009
Kavindu Herath <kavinduherath350@gmail.com>
2022-04-04 09:42:26 +0530

    seven

--- order.php
+++ order.php
@@ -7,6 +7,32 @@
         {
             //Get the food id and details of the selected food
             $food_id = $_GET['food_id'];
+            
+            //Get the details of the selected food
+            $sql = "SELECT * FROM tbl_food WHERE id=$food_id";
+
+            //Excecute the query
+            $res = mysqli_query($conn, $sql);
+
+            $count = mysqli_num_rows($res);
+
+            //Check whether data is available or not
+            if($count==1)
+            {
+                //We have data
+                //Get data from database
+                $row = mysqli_fetch_assoc($res);
+
+                $title = $row['title'];
+                $price = $row['price'];
+                $image_name = $row['image_name'];
+            }
+            else
+            {
+                //Food not available
+                //Redirect to home page
+                header('location:'.SITEURL);
+            }
         }
         else
         {
@@ -22,17 +48,38 @@
             
             <h2 class="text-center text-white">Fill this form to confirm your order.</h2>
 
-            <form action="#" class="order">
+            <form action="" method="POST" class="order">
                 <fieldset>
                     <legend>Selected Food</legend>
 
                     <div class="food-menu-img">
-                        <img src="images/menu-pizza.jpg" alt="Chicken Hawain Pizza" class="img-responsive img-curve">
+                        <?php 
+                        
+                        //Check whether the image is available or not
+                        if($image_name=="")
+                        {
+                            //Image not available
+                            echo "<div class='error'>Image not Available.</div>";
+                        }
+                        else
+                        {
+                            //Image not available
+                            ?>
+                                <img src="<?php echo SITEURL; ?>Images/food/<?php echo $image_name; ?>" alt="Chicken Hawain Pizza" class="img-responsive img-curve">
+
+                            <?php
+                        }
+                        
+                        ?>
+                        
                     </div>
     
                     <div class="food-menu-desc">
-                        <h3>Food Title</h3>
-                        <p class="food-price">$2.3</p>
+                        <h3><?php echo $title; ?></h3>
+                        <input type="hidden" name="food" value="<?php echo $title; ?>">
+
+                        <p class="food-price">$<?php echo $price; ?></p>
+                        <input type="hidden" name="price" value="<?php echo $price; ?>">
 
                         <div class="order-label">Quantity</div>
                         <input type="number" name="qty" class="input-responsive" value="1" required>
@@ -59,6 +106,70 @@
                 </fieldset>
 
             </form>
+
+            <?php
+            
+                    //Check whether submit button is clicked or not
+                    if(isset($_POST['submit']))
+                    {
+                        //Get all the details from the form
+
+                        $food = $_POST['food'];
+                        $price = $_POST['price'];
+                        $qty = $_POST['qty'];
+
+                        $total = $price * $qty; //total = price * qty
+
+                        $order_date = date("Y-m-d h:i:sa"); //Order date
+
+                        $status = "Ordered"; //Ordered, On delivery, Delivered, Cancelled
+
+                        $customer_name = $_POST['full-name'];
+                        $customer_contact = $_POST['contact'];
+                        $customer_email = $_POST['email'];
+                        $customer_address = $_POST['address'];
+
+                        //Save the order in database
+                        //Create sql to save data
+
+                        $sql2 = "INSERT INTO tbl_order SET
+                            food = '$food',
+                            price = '$price',
+                            qty = $qty,
+                            total = $total,
+                            order_date = '$order_date',
+                            status = '$status',
+                            customer_name = '$customer_name',
+                            customer_contact = '$customer_contact',
+                            customer_email = '$customer_email',
+                            customer_address = '$customer_address'
+                                                    
+                        ";
+
+
+                        //Excecute the query
+                        $res2 = mysqli_query($conn, $sql2);
+
+                        //Check whether query excecuted successfully or not
+                        if($res2==true)
+                        {
+                            //Query excecuted and order saved
+                            $_SESSION['order'] = "<div class='success text-center'>Food Ordered Successfully.</div>";
+                            header('location:'.SITEURL);
+
+                        }
+                        else
+                        {
+                            //Failed to save order
+                            $_SESSION['order'] = "<div class='error text-center'>Failed to Order Food.</div>";
+                            header('location:'.SITEURL);
+
+                        }
+
+
+                    }
+            
+            ?>
 
         </div>
     </section>
